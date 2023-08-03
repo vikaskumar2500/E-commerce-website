26dd1962152e976cf963c852294b779f6eb41c47
vikaskumar2500 <vikas.nits8084@gamil.com>
2023-08-03 11:57:24 +0530

    simply products are displayed on the screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const productsArr = [
+    {
+      title: "Colors",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+
+    {
+      title: "Black and white Colors",
+
+      price: 50,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+
+    {
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+
+    {
+      title: "Blue Color",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>COLORS</h1>
+      <ul>
+        {productsArr.map((product) => (
+          <li key={product.title}>
+            <h2>{product.title}</h2>
+            <img src={product.imageUrl} alt="images not found" />
+            <div className="info">
+              <div>${product.price}</div>
+              <button type="button">Add To Cart</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
